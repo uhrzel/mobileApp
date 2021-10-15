043ebe35749fd93d25e72ea54e3bdafefe489794
FlutLab.io <>
2021-10-15 02:51:47 +0000

    Autosave

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,45 +1,134 @@
 import 'package:flutter/material.dart';
-import 'dashboard.dart';
+import 'main.dart';
+import 'quiz.dart';
+import 'about_us.dart';
+import 'settings.dart';
+import 'rate.dart';
+import 'profile.dart';
 
-class Profile extends StatelessWidget {
-  // This widget is the root of your application.
+class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      debugShowCheckedModeBanner: false,
-      title: 'Settings Section',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: ProfileHome(title: 'User Profile'),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, theme: ThemeData(primarySwatch: Colors.pink), home: DashboardHome());
   }
 }
 
-class ProfileHome extends StatelessWidget {
-  final String title;
-
-  const ProfileHome({@required this.title});
-
+class DashboardHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Dashboard()));
-              }),
-          title: Text(title),
+      appBar: AppBar(
+        title: Text(
+          'Dashboard',
+          style: TextStyle(fontSize: 20.0, fontFamily: 'Mostery', color: Colors.black),
         ),
-        body: Container(
-            child: Text(
-          'Diri makita ang overview sang user',
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        )));
+        backgroundColor: Colors.pink,
+      ),
+      drawer: Drawer(
+          child: Card(
+              child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              'Select Options',
+              style: TextStyle(color: Colors.black, fontFamily: 'Juicer', fontSize: 30),
+            ),
+            tileColor: Colors.pink,
+          ),
+          Divider(
+            color: Colors.black,
+            height: 30.0,
+          ),
+          ListTile(
+              title: Text('QUIZ NOW!'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Home()));
+              },
+              tileColor: Colors.blue[900]),
+          Divider(
+            color: Colors.black,
+            height: 1.0,
+          ),
+          ListTile(
+              title: Text('HOME'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Dashboard()));
+              },
+              tileColor: Colors.blue[900]),
+          Divider(
+            color: Colors.black,
+            height: 0.5,
+          ),
+          ListTile(
+              title: Text('ABOUT US'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => About()));
+              },
+              tileColor: Colors.blue[900]),
+          Divider(
+            color: Colors.black,
+            height: 0.5,
+          ),
+          ListTile(
+              title: Text('QUIT'),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => MyApp()));
+              },
+              tileColor: Colors.blue[900]),
+          Divider(
+            color: Colors.black,
+            height: 0.5,
+          ),
+        ],
+      ))),
+      body: Container(
+          color: Colors.blue[900],
+          child: Center(
+              child: Column(children: <Widget>[
+            SizedBox(height: 70.0),
+            Text('Listahan ta sang nakasali', style: TextStyle(fontSize: 40.0, color: Colors.black)),
+            SizedBox(height: 20.0),
+          ]))),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue[900],
+        items: [
+          BottomNavigationBarItem(
+              icon: InkWell(
+                  child: Icon(
+                    Icons.settings,
+                    color: Colors.black,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Set()));
+                  }),
+              backgroundColor: Colors.red,
+              label: 'Settings'),
+          BottomNavigationBarItem(
+              icon: InkWell(
+                  child: Icon(
+                    Icons.grade,
+                    color: Colors.yellow,
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Rate()));
+                  }),
+              backgroundColor: Colors.yellow,
+              label: 'Ratings'),
+          BottomNavigationBarItem(
+              icon: InkWell(
+                  child: Icon(
+                    Icons.account_circle_rounded,
+                    color: Colors.green[400],
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => Profile()));
+                  }),
+              backgroundColor: Colors.pink,
+              label: 'Profile'),
+        ],
+        selectedItemColor: Colors.black54,
+      ),
+    );
   }
 }
