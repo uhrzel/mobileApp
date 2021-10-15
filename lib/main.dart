85a42be9a46dfe3070174bcc18ba9d73e48282b6
FlutLab.io <>
2021-10-15 06:17:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Hello World',
+      title: 'Mini System',
 // Application theme data, you can set the colors for the application as you want
       theme: ThemeData(
         primarySwatch: Colors.pink,
